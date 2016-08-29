6d243a47e98ccb894a309dddc9216441249db60a
xianfei689 <347560189@qq.com>
2016-08-29 11:43:41 +0800

    aa
    aa

---
+++ js/config.js
@@ -0,0 +1,26 @@
+
+/**
+ * [description]      seaJS 引用JS 配置路径
+ * @AuthorHTL                                                xianfei
+ * @DateTime  2016-06-23T08:34:33+0800
+ */
+//var  basepath = "/hg_phone_xianfei_v2.1"; local
+// var  basepath = "";
+//  conf [1:测试环境; 0 :本地环境]
+var conf = 0;
+var srcPref = ["/saleOnline-h5-hg-v1.0",""];
+var timeStamp = '$1?ver=' + new Date().getTime();
+seajs.config({
+  'map': [
+		[ /^(.*\.(?:css|js))(.*)$/i,  timeStamp ]
+  ],
+  base: srcPref[conf],
+  // 设置路径，方便跨目录调用
+  paths: {
+    //'phoneUI': '../../../../frame/lib/webui/1.0.0/phone/js'
+  },
+  // 设置别名，方便调用
+  alias:  {
+	 'rose': 'frame/lib/Rose/1.0.0/Rose.src'
+  }
+});
---
+++ js/home.js
@@ -0,0 +1,36 @@
+;
+/**
+ * [description]
+ * ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥
+ *
+ * @Author                   ------>                                                         xianfei
+ * @Title★★★★★★★
+ * @DateTime------>                                            2016-08-29T11:36:59+0800
+ * @description [saleonline  main js]
+ *
+ * ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥
+ * @param           {[type]} require                           [description]
+ * @param           {[type]} exports                           [description]
+ * @param           {Object} module)                           {	var                   home [description]
+ * @param           {[type]} _init_sale_login:function(){		} [description]
+ * @param           {[type]} };	module.exports                [description]
+ * @return          {[type]}                                   [description]
+ */
+define(function(require, exports, module) {
+
+	var home = {
+
+		init:function(){
+			/**
+			 * 加载页面开始
+			 */
+			//this._init_sale_login();
+		},
+		_init_sale_login:function(){
+
+		},
+
+
+	};
+	module.exports = home;
+});
